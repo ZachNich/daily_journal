df6a4a065f50c7000c07591e7260dd8bb6cdcaa2
Zach Nicholson <raiste.doctow@gmail.com>
2020-04-17 15:42:54 -0500

    added JS exercise

---
+++ scripts/journal.js
@@ -0,0 +1,38 @@
+const journalEntry_One = {
+    date: {
+        day: 17,
+        month: 'April',
+        year: 2020
+    },
+    concepts: "Javscript Objects",
+    entry: "Learned to construct and access JS objects. Also learned to use .querySelector to access and modify DOM.",
+    mood: "Optomistic"
+}
+
+const journalEntry_Two = {
+    date: {
+        day: 17,
+        month: 'April',
+        year: 2020
+    },
+    concepts: "HTML/CSS",
+    entry: "Learned how to use flexbox and other CSS tools to style an HTML page. Also learned how to create an HTML skeleton with wireframing and sections before using CSS to style.",
+    mood: "Energetic"
+}
+
+const journalEntry_Three = {
+    date: {
+        day: 17,
+        month: 'April',
+        year: 2020
+    },
+    concepts: "Teamwork, Presenting, GitHub",
+    entry: "Learned how to discuss/plan/produce as a team with different ideas & skills. Also put together quick presentation with group and struggled through learning GitHub flow with pushes and pulls in a group setting.",
+    mood: "Ovewhelmed"
+}
+
+const journalEntries = [];
+
+journalEntries.push(journalEntry_One);
+journalEntries.push(journalEntry_Two);
+journalEntries.push(journalEntry_Three);
