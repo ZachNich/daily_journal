2bf3fe5cbaa24ec1080a456e6df77d871f139635
Zach Nicholson <raiste.doctow@gmail.com>
2020-05-05 11:35:45 -0500

    DJ7 complete, receive user input/add to database/insert to DOM

--- scripts/entriesDOM.js
+++ scripts/entriesDOM.js
@@ -1,4 +1,5 @@
 import entryComponent from './entryComponent.js'
+
 const renderJournalEntries = (entries) => {
     entries.forEach(entry => {
         document.querySelector('.entryLog').innerHTML += entryComponent.makeJournalEntryComponent(entry);
